a2eda7c702d8bff599d2fa68e196ae4864fd2765
Ruben2099 <rsivajoti@gmail.com>
2025-01-27 21:59:18 +0000

    Update Ultrasonic.ino

--- car/Top board/Ultrasonic/Ultrasonic.ino
+++ car/Top board/Ultrasonic/Ultrasonic.ino
@@ -12,6 +12,7 @@ const int res = 8;
 const int ledRChannel = 0;
 const int ledGChannel = 1;
 
+float lastscantime;
 long duration;
 int distance;
 
@@ -21,37 +22,42 @@ void setup() {
     pinMode(echo, INPUT);
     pinMode(ledGPin, OUTPUT);
     pinMode(ledRPin, OUTPUT);
-    ledcAttachPin(ledRPin, ledRChannel);
-    ledcAttachPin(ledGPin, ledGChannel);
+    ledcAttach(ledRPin, freq, res);
+    ledcAttach(ledGPin, freq, res);
 }
 
 void loop() {
-    distanceUpdate();
-    // Debugging output
-    Serial.print("Distance: ");
-    Serial.println(distance);
-  
+  distancecheck();
 }
 
-void distanceUpdate(){
-  // scan 10/s or every 100ms
-  if(Lastscantime - millis() <=100){
-  // Checking and updating HC-SR04 distance
+void distancecheck(){
+
+  if(lastscantime - millis() <= 200){
+    //triggering scan
     digitalWrite(trig, LOW);
     delayMicroseconds(2);
     digitalWrite(trig, HIGH);
     delayMicroseconds(10);
     digitalWrite(trig, LOW);
+      
     duration = pulseIn(echo, HIGH, 50000); // Timeout of 50ms
+    // calculating distance
     distance = duration * 0.034 / 2;
-    Lastscantime = millis()
-  // Adjusting LED brightness based on distance
+    lastscantime = millis();
+  // Calculate PWM value based on distance, use floating-point division
+    float pwm = 355 * (1.0 / distance); // 355 used for higher voltage to led - better visability, less strobing due to distance change
+    pwm = constrain(pwm, 0, 255);  // Clamp PWM to 0-255 range
+
+    // Adjusting LED brightness based on distance
     if (distance <= 10) {
-      pwm = 255*(1/distance);
-      ledcWrite(ledRPin, pwm);
+        ledcWrite(ledRPin, pwm);  // Use the correct channel for the red LED
+    } else {
+        ledcWrite(ledRPin, LOW);  // Turn off the red LED
     }
-    else{
-      ledcWrite(ledRPin, LOW);
-    } 
+    
+    // Debugging output
+    Serial.print("Distance: ");
+    Serial.println(distance);
   }
+
 }

930c3fec28130758225fd17b10515c93917145b3
Ruben2099 <rsivajoti@gmail.com>
2025-01-27 23:52:18 +0000

    Update MPU6050eg.ino

--- car/Top board/MPU6050eg/MPU6050eg.ino
+++ car/Top board/MPU6050eg/MPU6050eg.ino
@@ -3,32 +3,52 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
-const float Xoffset = 0.51;
-const float Yoffset = -0.16;
-float AccX;
-float AccY;
-float turnangle;
 
 Adafruit_MPU6050 mpu;
+float xoffset = 0.52;
+float yoffset = -0.16;
+float accX;
+float accY;
+float turnangle;
 
 void setup(void) {
   Serial.begin(115200);
   while (!Serial)
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  delay(100);
 }
 
 void loop() {
-   updateMPU();
-}
 
-void updateMPU(){
- /* Get new sensor events with the readings */
+  /* Get new sensor events with the readings */
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-  AccX = a.acceleration.x - Xoffset;
-  AccY = a.acceleration.y - Yoffset;
-  turnangle = atan2(AccY,AccX);
-  Serial.print(AccX);
-  Serial.println(AccY);
-  Serial.print(turrnangle);
+
+  /* Print out the values */
+  accX = a.acceleration.x - xoffset;
+  accY = a.acceleration.y - yoffset;
+  turnangle = atan2(accY,accX);
+  Serial.print("Acceleration X: ");
+  Serial.println(accX);
+  Serial.print(", Y: ");
+  Serial.println(accY);
+  Serial.print(", angle: ");
+  Serial.println(turnangle);
+
+  delay(200);
 }
